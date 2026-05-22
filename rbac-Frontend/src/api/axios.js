888d0c29887b08f928c61efeb941d8e4ec8acc69
ANUJ KUMAR <anuj1994k>
2026-05-22 22:58:34 +0530

    dotenv used

--- rbac-Frontend/src/api/axios.js
+++ rbac-Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://avidus-assignment-1.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
